991ad6e6fe2255c857f44ca6375fbb7b2ec0c982
AviDoesDev <81582444+AviDoesDev@users.noreply.github.com>
2021-07-19 14:13:53 +0530

    Update types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -149,3 +149,35 @@ export interface NexusStats {
     },
     uptime: 300
 }
+
+export type FiltersName = keyof QueueFilters;
+
+export type QueueFilters = {
+    bassboost?: boolean;
+    '8D'?: boolean;
+    vaporwave?: boolean;
+    nightcore?: boolean;
+    phaser?: boolean;
+    tremolo?: boolean;
+    vibrato?: boolean;
+    reverse?: boolean;
+    treble?: boolean;
+    normalizer?: boolean;
+    surrounding?: boolean;
+    pulsator?: boolean;
+    subboost?: boolean;
+    karaoke?: boolean;
+    flanger?: boolean;
+    gate?: boolean;
+    haas?: boolean;
+    mcompand?: boolean;
+    mono?: boolean;
+    mstlr?: boolean;
+    mstrr?: boolean;
+    compressor?: boolean;
+    expander?: boolean;
+    softlimiter?: boolean;
+    chorus?: boolean;
+    chorus2d?: boolean;
+    chorus3d?: boolean;
+    
